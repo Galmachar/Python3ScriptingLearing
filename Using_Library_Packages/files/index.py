c7c74d2585010ffb7ab9837fd3278889f4c538ed
galmachar <grzegduel@gmail.com>
2020-04-20 16:42:28 +0200

    some of experimenting with files

--- Using_Library_Packages/files/index.py
+++ Using_Library_Packages/files/index.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python
+import os
+path = "/home/grzesiek/Python3_learing_admin/Using_Library_Packages/files/ducks.txt"
+ducks_file = open(path, "r", encoding="utf=8")
+
+new_ducks = open('new_ducks.txt', 'a')
+
+new_ducks.write(ducks_file.read())
