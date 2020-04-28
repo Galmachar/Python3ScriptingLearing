192f4a2273ee50eef18159e1ee58fef817c5f9b9
galmachar <grzegduel@gmail.com>
2020-04-28 12:41:22 +0200

    finishing script revesing everything in file

--- Parsin_Command_Line/reverse-file.py
+++ Parsin_Command_Line/reverse-file.py
@@ -9,8 +9,13 @@ parser.add_argument('--limit', '-l', type=int, help='number of lines to read')
 parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 args = parser.parse_args()
-print(args)
 
-# parse arguments
+with open(args.filename) as f:
+    lines = f.readlines()
+    lines.reverse()
 
-# reat the file, reverse the contents and print
+    if args.limit: #if limit exists
+        lines = lines[:args.limit]
+    
+    for line in lines:
+        print(line.strip()[::-1]) #this is how to reverse string in python!
