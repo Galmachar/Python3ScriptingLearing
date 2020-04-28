0e102924b853f93b54ee2d2ae712df7fcc477c67
galmachar <grzegduel@gmail.com>
2020-04-28 12:48:49 +0200

    adding error handling to reverse file script

---
+++ Reserse_File_Script/parametr.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import sys
+
+# argparse - argumenty, flagi, dokumentacja.
+print(f'Positional arguments: {sys.argv[1:]}')
+print(f'First agument {sys.argv[0]}')
---
+++ Reserse_File_Script/reverse-file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import argparse
+
+# build the parser
+
+parser = argparse.ArgumentParser(description='Reads a file in reverse')
+parser.add_argument('filename', help='file to read')
+parser.add_argument('--limit', '-l', type=int, help='number of lines to read')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+args = parser.parse_args()
+
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+        if args.limit: #if limit exists
+            lines = lines[:args.limit]
+        for line in lines:
+            print(line.strip()[::-1]) #this is how to reverse string in python!
